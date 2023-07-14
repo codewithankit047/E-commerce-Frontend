bc1afc03fb937435c45760fddbac86716cedd16d
Ankit <111271551+codewithankit047@users.noreply.github.com>
2023-07-14 13:06:03 +0530

    navbar + route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./Component/Header";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-     <h2>E-commerce APP</h2>
-    </div>
+    <>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<h1>E-commerce home page</h1>} />
+          <Route path="/add" element={<h1>Add product component page</h1>} />
+          <Route
+            path="/update"
+            element={<h1>update product component page</h1>}
+          />
+          <Route path="/logout" element={<h1> Logout page</h1>} />
+          <Route path="/profile" element={<h1> profile page</h1>} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
---
+++ src/Component/Header/index.jsx
@@ -0,0 +1,106 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            E-Commerce
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <Link to='/' className="nav-link active" aria-current="page"  >
+                  Home
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to='/add' className="nav-link active" aria-current="page"  >
+                  Add Products
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to='/update' className="nav-link active" aria-current="page"  >
+                  Update Products
+                </Link>
+              </li>
+              <li className="nav-item ">
+                <Link to='/logout' className="nav-link active" aria-current="page"  >
+                Logout
+                </Link>
+              </li>
+              <li className="nav-item ">
+                <Link to='/profile' className="nav-link active" aria-current="page"  >
+                Profile
+                </Link>
+              </li>
+              {/* <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Link
+                </a>
+              </li> */}
+              {/* <li className="nav-item dropdown">
+                <a
+                  className="nav-link dropdown-toggle"
+                  href="#"
+                  id="navbarDropdown"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Dropdown
+                </a>
+                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Action
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Another action
+                    </a>
+                  </li>
+                  <li>
+                    <hr className="dropdown-divider" />
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Something else here
+                    </a>
+                  </li>
+                </ul>
+              </li> */}
+            </ul>
+            <form className="d-flex">
+              <input
+                className="form-control me-2"
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+              />
+              <button className="btn btn-outline-success" type="submit">
+                Search
+              </button>
+            </form>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Header;
